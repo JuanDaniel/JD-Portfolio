58b5f02e23e943d59825c2b4788f8df0d64bca66
Juan Daniel SANTANA <juandanielsantana@gmail.com>
2022-03-24 17:48:09 -0400

    Se agregaron los componentes AppHeader, AppFooter. Se definió el layout portfolio. Se integró el google-fonts package.

---
+++ layouts/portfolio.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="wrapper">
+    <app-header />
+    <nuxt />
+    <app-footer />
+  </div>
+</template>
+
+<script>
+import AppHeader from '~/components/AppHeader.vue'
+import AppFooter from '~/components/AppFooter.vue'
+
+export default {
+  components: {
+    AppHeader,
+    AppFooter
+  }
+}
+</script>
